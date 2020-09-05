f20a6bfe3e4e3492be78f39ed909a8224699e257
Artem Ovdienko <ovd.artem92@gmail.com>
2020-09-05 11:01:09 +0300

    Tanks 1.0

---
+++ src/display/Display.java
@@ -0,0 +1,27 @@
+package display;
+
+import javax.swing.*;
+import java.awt.*;
+
+public abstract class Display {
+    private static boolean created = false;
+    private static JFrame window;
+    private static Canvas content;
+
+    public static void created(int width, int height, String title) {
+        if (created)
+            return;
+
+        window = new JFrame(title);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        content = new Canvas();
+        Dimension size = new Dimension(width, height);
+        content.setPreferredSize(size);
+        window.setResizable(false);
+        window.getContentPane().add(content);
+        window.pack();
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+
+    }
+}
---
+++ src/main/Main.java
@@ -0,0 +1,9 @@
+package main;
+
+import display.Display;
+
+public class Main {
+    public static void main(String[] args) {
+        Display.created(800, 600, "tanks");
+    }
+}
