8547b84a80419682a2f35aaf67e21cf6ac371ad0
Artem Ovdienko <ovd.artem92@gmail.com>
2020-09-05 12:25:43 +0300

    Tanks 1.0

--- src/display/Display.java
+++ src/display/Display.java
@@ -2,40 +2,57 @@ package display;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
+import java.util.Arrays;
 
 public abstract class Display {
+
     private static boolean created = false;
     private static JFrame window;
     private static Canvas content;
 
-    public static void created(int width, int height, String title) {
+    private static BufferedImage buffer;
+    private static int[] bufferData;
+    private static Graphics bufferGraphics;
+    private static int clearColor;
+
+    private static float delta = 0;
+
+    public static void created(int width, int height, String title, int _clearColor) {
         if (created)
             return;
 
         window = new JFrame(title);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        content = new Canvas(){
-
-            public void paint(Graphics g){
-                super.paint(g);
-                render(g);
-            }
-        };
+        content = new Canvas();
         Dimension size = new Dimension(width, height);
         content.setPreferredSize(size);
-        content.setBackground(Color.BLACK);
         window.setResizable(false);
         window.getContentPane().add(content);
         window.pack();
         window.setLocationRelativeTo(null);
         window.setVisible(true);
 
+        buffer = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+        bufferData = ((DataBufferInt) buffer.getRaster().getDataBuffer()).getData();
+        bufferGraphics = buffer.getGraphics();
+        clearColor = _clearColor;
+
+
+        created = true;
+    }
+
+    public static void clear() {
+        Arrays.fill(bufferData, clearColor);
     }
     public static void render() {
-        content.repaint();
+        bufferGraphics.setColor(new Color(0xff0000ff));
+        bufferGraphics.fillOval((int)(350 + (Math.sin(delta) * 200)), 250, 100, 100);
+        delta += 0.02f;
     }
-    private static void render(Graphics g) {
-        g.setColor(Color.white);
-        g.fillOval(400 - 50, 300 - 50, 100, 100);
+    public static void swapBuffers() {
+        Graphics g = content.getGraphics();
+        g.drawImage(buffer, 0, 0, null);
     }
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -7,13 +7,15 @@ import java.awt.event.ActionEvent;
 
 public class Main {
     public static void main(String[] args) {
-        Display.created(800, 600, "Tanks. Since 1992");
+        Display.created(800, 600, "Tanks. Since 1992", 0xff00ff00);
         Timer t = new Timer(1000 / 60, new AbstractAction(
 
         ) {
             @Override
             public void actionPerformed(ActionEvent e) {
+                Display.clear();
                 Display.render();
+                Display.swapBuffers();
             }
         });
         t.setRepeats(true);
