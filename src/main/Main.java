a26c6bfbadc9f106b3c053bf919e56073ed411c1
Artem Ovdienko <ovd.artem92@gmail.com>
2020-09-05 11:23:02 +0300

    Tanks 1.0

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,8 +2,21 @@ package main;
 
 import display.Display;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+
 public class Main {
     public static void main(String[] args) {
-        Display.created(800, 600, "tanks");
+        Display.created(800, 600, "Tanks. Since 1992");
+        Timer t = new Timer(1000 / 60, new AbstractAction(
+
+        ) {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Display.render();
+            }
+        });
+        t.setRepeats(true);
+        t.start();
     }
 }
